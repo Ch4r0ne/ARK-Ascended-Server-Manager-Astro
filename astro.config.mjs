2adedc22c445abcc261f263b4bfa8b085b8f1dc9
Ch4r0ne <54702161+Ch4r0ne@users.noreply.github.com>
2026-01-20 19:20:43 +0100

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,6 +3,7 @@ import react from '@astrojs/react';
 import tailwindcss from '@tailwindcss/vite';
 
 export default defineConfig({
+  site: 'https://asa-manager.technetpro.de',
   integrations: [react()],
   vite: { plugins: [tailwindcss()] },
   server: {
